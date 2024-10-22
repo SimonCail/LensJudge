dec35d7fdc40213c29c930f3b41aafd89499cc8b
Mathias92-cmd <mathias_morel@ens.univ-artois.fr>
2024-10-22 11:11:47 +0200

    Fixes #16 - Creation de la classe CompilerJava avec ses methodes (surement incomplete pour le moment)

---
+++ app/src/main/java/lensjudge/compilation/CompilerJava.java
@@ -0,0 +1,25 @@
+package lensjudge.compilation;
+
+import java.io.IOException;
+
+public class CompilerJava extends CompilerStrategy{
+
+    @Override
+    public boolean isCompatible(String languageSelected) {
+        return languageSelected.equals("java");
+    }
+
+    @Override
+    public String getBinaryFileName(String sourceFileName) {
+        return sourceFileName.replace(".java", ".class");
+    }
+
+    @Override
+    public void executeCompilerCommand(String sourceFileName, String binaryFileName) throws IOException, InterruptedException {
+        ProcessBuilder processBuilder = new ProcessBuilder("javac", "-d", "compilation", sourceFileName);
+        processBuilder.inheritIO();
+        Process process = processBuilder.start();
+        process.waitFor();
+
+    }
+}
