309bc469fcaadf81ca64b182f4d4669b7e591c38
Mathias92-cmd <mathias_morel@ens.univ-artois.fr>
2024-10-22 13:01:49 +0200

    Fixes #17 - Creation de la classe CompilerPython

---
+++ app/src/main/java/lensjudge/compilation/CompilerPython.java
@@ -0,0 +1,26 @@
+package lensjudge.compilation;
+
+public class CompilerPython extends CompilerStrategy {
+
+    @Override
+    public boolean isCompatible(String languageSelected) {
+        return languageSelected.equals("python");
+    }
+
+    @Override
+    public String getBinaryFileName(String sourceFileName) {
+        return sourceFileName;
+    }
+
+    @Override
+    public void executeCompilerCommand(String sourceFileName, String binaryFileName) {
+        ProcessBuilder processBuilder = new ProcessBuilder("python3", "-m", "py_compile", sourceFileName);
+        processBuilder.inheritIO();
+        try {
+            Process process = processBuilder.start();
+            process.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
