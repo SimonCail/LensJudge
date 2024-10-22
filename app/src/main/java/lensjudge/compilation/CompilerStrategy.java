f56835e5106baf7682a267428caca26054d61b10
Mathias92-cmd <mathias_morel@ens.univ-artois.fr>
2024-10-22 10:39:31 +0200

    Fixes #15 - Creation de l'interface ICompilerStrategy

--- app/src/main/java/lensjudge/compilation/CompilerStrategy.java
+++ app/src/main/java/lensjudge/compilation/CompilerStrategy.java
@@ -2,11 +2,7 @@ package lensjudge.compilation;
 
 import java.io.IOException;
 
-public abstract class CompilerStrategy {
-    protected abstract boolean isCompatible(String languageSelected);
-    protected abstract String getBinaryFileName(String sourceFileName);
-    protected abstract void executeCompilerCommand(String sourceFileName, String binaryFileName) throws IOException , InterruptedException;
-
+public abstract class CompilerStrategy implements ICompilerStrategy {
     public void compile(String sourceFileName, String languageSelected) throws IOException, InterruptedException {
         if (isCompatible(languageSelected)) {
             String binaryFileName = getBinaryFileName(sourceFileName);
