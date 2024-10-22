512245fe3c2a80da37dce7510969ed458b428557
Mathias92-cmd <mathias_morel@ens.univ-artois.fr>
2024-10-22 13:13:48 +0200

    Fixes #19 - Creation de la classe CompilerCPP

---
+++ app/src/main/java/lensjudge/compilation/CompilerCPP.java
@@ -0,0 +1,26 @@
+package lensjudge.compilation;
+
+public class CompilerCPP extends CompilerStrategy {
+
+    @Override
+    public boolean isCompatible(String languageSelected) {
+        return languageSelected.equals("cpp");
+    }
+
+    @Override
+    public String getBinaryFileName(String sourceFileName) {
+        return "exe";
+    }
+
+    @Override
+    public void executeCompilerCommand(String sourceFileName, String binaryFileName) {
+        ProcessBuilder processBuilder = new ProcessBuilder("g++", "-x", "c++" , "-Wall" , "-02" , "-pipe" , "-lm" , "-o" , binaryFileName, sourceFileName);
+        processBuilder.inheritIO();
+        try {
+            Process process = processBuilder.start();
+            process.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
