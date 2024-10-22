88fdd7dc912663797887200c9a68f48affc682f4
benjamin_cornet <benjamin_cornet@ens.univ-artois.fr>
2024-10-22 10:12:30 +0200

    Fixes #6 - Ajout de l'interface permettant de contrôler un processus

---
+++ app/src/main/java/lensjudge/process/IControlProcess.java
@@ -0,0 +1,11 @@
+package lensjudge.process;
+
+import java.io.IOException;
+
+public interface IControlProcess {
+    String getErrorOutput() throws IOException;
+    String getStandardOutput() throws IOException;
+    void startProcess(String... command) throws IOException;
+    int waitForProcess() throws InterruptedException;
+    void stopProcess();
+}
