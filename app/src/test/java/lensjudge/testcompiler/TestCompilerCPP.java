7555dc4f230c890350a20d3b94cf20942616f87c
Mathias92-cmd <mathias_morel@ens.univ-artois.fr>
2024-10-22 14:51:29 +0200

    Fixes #28 - Creation de la classe TestCompilerCPP

---
+++ app/src/test/java/lensjudge/testcompiler/TestCompilerCPP.java
@@ -0,0 +1,41 @@
+package lensjudge.testcompiler;
+
+import lensjudge.compilation.CompilerCPP;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestCompilerCPP {
+    @Test
+    @DisplayName("Test isCompatible()")
+    public void testIsCompatible() {
+        CompilerCPP compilerCPP = new CompilerCPP();
+        assertTrue(compilerCPP.isCompatible("cpp"));
+        assertFalse(compilerCPP.isCompatible("java"));
+    }
+
+    @Test
+    @DisplayName("Test getBinaryFileName()")
+    public void testGetBinaryFileName() {
+        CompilerCPP compilerCPP = new CompilerCPP();
+        String sourceFileName = "test.cpp";
+        String expected = "test.exe";
+        String actual = compilerCPP.getBinaryFileName(sourceFileName);
+        assertTrue(expected.equals(actual));
+    }
+
+    @Test
+    @DisplayName("Test executeCompilerCommand()")
+    public void testExecuteCompilerCommand() {
+        CompilerCPP compilerCPP = new CompilerCPP();
+        String sourceFileName = "/export/etu/mathias.morel/Documents/BUT2/SAE/groupe-b2-s3.a.01/ProgTestSae/timethroughtheglass.cc";
+        String binaryFileName = "/export/etu/mathias.morel/Documents/BUT2/SAE/groupe-b2-s3.a.01/ProgTestSae/timethroughtheglass.exe";
+        try {
+            compilerCPP.executeCompilerCommand(sourceFileName, binaryFileName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
