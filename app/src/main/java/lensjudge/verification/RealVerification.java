212266938fa531d8ea96071ad5869d9432339651
matheo.dalmasso <matheo.dalmasso@info0627.ipa.iutlens.local>
2024-10-22 14:50:21 +0200

    Fixes #26 - Creation de la classe RealVerification

--- app/src/main/java/lensjudge/verification/RealVerification.java
+++ app/src/main/java/lensjudge/verification/RealVerification.java
@@ -1,6 +1,9 @@
 package lensjudge.verification;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class RealVerification implements IVerification {
     @Override
@@ -20,6 +23,33 @@ public class RealVerification implements IVerification {
 
     @Override
     public boolean verify(String pathFileIn, String pathFileOut) {
-        return true;
+        if (isNotEmpty(pathFileIn) && isNotEmpty(pathFileOut)) {
+            try {
+                File fileIn = new File(pathFileIn);
+                File fileOut = new File(pathFileOut);
+                BufferedReader br = new BufferedReader(new FileReader(fileIn));
+                BufferedReader br2 = new BufferedReader(new FileReader(fileOut));
+                String lineIn;
+                String lineOut;
+                float[] arrIn = new float[10];
+                float[] arrOut = new float[10];
+                int compt = 0;
+                if (fileIn.length() != fileOut.length()) {
+                    return false;
+                }
+                while ((lineIn = br.readLine()) != null && (lineOut = br2.readLine()) != null) {
+                    arrIn[compt] = Float.parseFloat(lineIn);
+                    arrOut[compt] = Float.parseFloat(lineOut);
+                    compt++;
+                    if (arrIn[compt] - arrOut[compt] >= 0.00001 || arrIn[compt] - arrOut[compt] <= -0.00001) {
+                        return false;
+                    }
+                }
+                return true;
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+        return false;
     }
 }
