6afceba690247b81e490cb0a538ef16362d25252
Mathias92-cmd <mathias_morel@ens.univ-artois.fr>
2024-10-23 17:26:37 +0200

    Ajout d'un deuxieme test dans runnerMain

--- app/src/main/java/lensjudge/MainRunner.java
+++ app/src/main/java/lensjudge/MainRunner.java
@@ -13,5 +13,9 @@ public class MainRunner {
         TestCase testCase = new TestCase("test", new File("/export/etu/mathias.morel/Documents/BUT2/SAE/groupe-b2-s3.a.01/ProgTestSae/runnerTest"), new File("/export/etu/mathias.morel/Documents/BUT2/SAE/groupe-b2-s3.a.01/ProgTestSae/runnerTest"));
         Runner runner = new Runner(new File("/export/etu/mathias.morel/Documents/BUT2/SAE/groupe-b2-s3.a.01/ProgTestSae/timethroughtheglass.cc"), testCase);
         runner.run();
+        System.out.println("--------------------------------------------------------------------------");
+        TestCase testCase2 = new TestCase("test2" , new File("/export/etu/mathias.morel/Documents/BUT2/SAE/groupe-b2-s3.a.01/ProgTestSae/runnerTest2"), new File("/export/etu/mathias.morel/Documents/BUT2/SAE/groupe-b2-s3.a.01/ProgTestSae/runnerTest2"));
+        Runner runner2 = new Runner(new File("/export/etu/mathias.morel/Documents/BUT2/SAE/groupe-b2-s3.a.01/ProgTestSae/timethroughtheglass.cc"), testCase2);
+        runner2.run();
     }
 }
