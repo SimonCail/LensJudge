8771feb4159fb08741bbf150d8c4d3d18aab7dc7
benjamin_cornet <benjamin_cornet@ens.univ-artois.fr>
2024-10-23 17:22:25 +0200

    Avancement sur l'execution de java

--- app/src/main/java/lensjudge/Execution/ExecutionJava.java
+++ app/src/main/java/lensjudge/Execution/ExecutionJava.java
@@ -11,8 +11,8 @@ import java.io.InputStreamReader;
 
 public class ExecutionJava implements IExecution {
     public ProcessAdapter execute(String sourceFilePath, String binaryFileName) throws IOException, InterruptedException {
-        ProcessAdapter processAdapter=new ProcessAdapter(binaryFileName);
-        processAdapter.startProcess();
+        String className = binaryFileName.replace(".class", "");
+        ProcessAdapter processAdapter = new ProcessAdapter("java", className);
         return processAdapter;
     }
 }
--- app/src/main/java/lensjudge/Main2.java
+++ app/src/main/java/lensjudge/Main2.java
@@ -6,20 +6,36 @@ import lensjudge.Execution.ExecutionJava;
 import lensjudge.Execution.ExecutionPy;
 import lensjudge.compilation.CompilerC;
 import lensjudge.compilation.CompilerCPP;
+import lensjudge.compilation.CompilerJava;
 import lensjudge.compilation.CompilerPython;
 import lensjudge.process.ProcessAdapter;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Main2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         ProcessAdapter process;
         ProcessAdapter process2;
         ProcessAdapter process3;
         ProcessAdapter process4;
 
+        ExecutionJava executionJava = new ExecutionJava();
+        String sourceFilePath4 = "C:/Users/Benji/Documents/COUR/BUT2/SAEA3.01/groupe-b2-s3.a.01/ProgTestSae/TimeThroughTheGlass.java";
+        File sourceFile4=new File(sourceFilePath4);
+        CompilerJava compilerJava=new CompilerJava();
+        String className = compilerJava.getBinaryFileName(sourceFilePath4);
+        compilerJava.executeCompilerCommand(sourceFile4,null);
+        process4=executionJava.execute(sourceFilePath4, className);
+        process4.startProcess();
+        System.out.println(process4.getStandardOutput());
+        System.out.println("--------------------------------------------------------------------------------");
+    }
+}
+
+/*
         ExecutionPy executionPy = new ExecutionPy();
-        String sourceFilePath2 = "/export/etu/simon.caillieret/Documents/QDEV/SAE/groupe-b2-s3.a.01/ProgTestSae/timethroughtheglass.py";
+        String sourceFilePath2 = "C:/Users/Benji/Documents/COUR/BUT2/SAEA3.01/groupe-b2-s3.a.01/ProgTestSae/timethroughtheglass.py";
         File sourceFile2=new File(sourceFilePath2);
         CompilerPython compilerPython = new CompilerPython();
         compilerPython.executeCompilerCommand(sourceFile2, null);
@@ -29,7 +45,7 @@ public class Main2 {
         System.out.println("-----------------------------------------------------------------------------------");
 
         ExecutionC executionC = new ExecutionC();
-        String sourceFilePath3 = "/export/etu/simon.caillieret/Documents/QDEV/SAE/groupe-b2-s3.a.01/ProgTestSae/timethroughtheglassC.c";
+        String sourceFilePath3 = "C:/Users/Benji/Documents/COUR/BUT2/SAEA3.01/groupe-b2-s3.a.01/ProgTestSae/timethroughtheglassC.c";
         File sourceFile3=new File(sourceFilePath3);
         CompilerC compilerC=new CompilerC();
         String binaryFileName2;
@@ -41,26 +57,14 @@ public class Main2 {
         System.out.println("--------------------------------------------------------------------------------");
 
         ExecutionCPP executionCPP = new ExecutionCPP();
-        String sourceFilePath = "/export/etu/simon.caillieret/Documents/QDEV/SAE/groupe-b2-s3.a.01/ProgTestSae/timethroughtheglass.cc";
+        String sourceFilePath = "C:/Users/Benji/Documents/COUR/BUT2/SAEA3.01/groupe-b2-s3.a.01/ProgTestSae/timethroughtheglass.cc";
         File sourceFile=new File(sourceFilePath);
         CompilerCPP compilerCPP=new CompilerCPP();
-        String binaryFileName;
-        binaryFileName= compilerCPP.getBinaryFileName(sourceFilePath);
-        compilerCPP.executeCompilerCommand(sourceFile, binaryFileName);
-        process=executionCPP.execute(sourceFilePath, binaryFileName);
+        String binaryFileName3;
+        binaryFileName3= compilerCPP.getBinaryFileName(sourceFilePath);
+        compilerCPP.executeCompilerCommand(sourceFile, binaryFileName3);
+        process=executionCPP.execute(sourceFilePath, binaryFileName3);
         process.startProcess();
         System.out.println(process.getStandardOutput());
         System.out.println("--------------------------------------------------------------------------------");
-    }
-}
-
-/*
-        ExecutionJava executionJava = new ExecutionJava();
-        String sourceFilePath4 = "/export/etu/simon.caillieret/Documents/QDEV/SAE/groupe-b2-s3.a.01/ProgTestSae/timethroughtheglass.cc";
-        process4=executionCPP.execute(sourceFilePath4);
-        System.out.println(process4.getStandardOutput());
-        System.out.println("--------------------------------------------------------------------------------");
-
  */
-
-
