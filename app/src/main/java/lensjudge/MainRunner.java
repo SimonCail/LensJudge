4552c43fd69d37311b9b9a74f671b6aa13a5fdfc
Mathias92-cmd <mathias_morel@ens.univ-artois.fr>
2024-10-23 16:01:29 +0200

    Changement dans les classes MainRunner et IRunnerBuilder

--- app/src/main/java/lensjudge/MainRunner.java
+++ app/src/main/java/lensjudge/MainRunner.java
@@ -1,20 +1,13 @@
 package lensjudge;
 
+import lensjudge.runnerbuilder.IRunnerBuilder;
 import lensjudge.runnerbuilder.Runner;
 import lensjudge.runnerbuilder.RunnerBuilder;
 
 import java.io.File;
 
 public class MainRunner {
-    public static void main(String[] args){
-        Runner runner = new RunnerBuilder()
-                .setSourceFile(new File("ProgTestSae/test.c"))
-                .setInputFileName("ProgTestSae/2.in")
-                .setOutputFileName("ProgTestSae/2.ans")
-                .build();
-
-        String result = runner.run();
-        System.out.println("Result: " + result);
+    public static void main(String[] args) {
 
     }
 }
--- app/src/main/java/lensjudge/runnerbuilder/IRunnerBuilder.java
+++ app/src/main/java/lensjudge/runnerbuilder/IRunnerBuilder.java
@@ -1,10 +1,15 @@
 package lensjudge.runnerbuilder;
 
+import lensjudge.problem.ConstructProblem;
+import lensjudge.problem.TestCase;
+
 import java.io.File;
+import java.io.IOException;
 
 public interface IRunnerBuilder {
-    RunnerBuilder setSourceFile(File sourceFile);
-    RunnerBuilder setInputFileName(String inputFileName);
-    RunnerBuilder setOutputFileName(String outputFileName);
-    Runner build();
+    void setTestCase(TestCase testCase);
+    void setConstructProblem(ConstructProblem constructProblem);
+    TestCase getTestCase();
+    ConstructProblem getConstructProblem();
+    void run() throws IOException;
 }
