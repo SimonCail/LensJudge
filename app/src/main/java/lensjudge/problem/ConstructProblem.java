987b50a65ef782c3dd98729f59dfcab77f90fe6d
benjamin_cornet <benjamin_cornet@ens.univ-artois.fr>
2024-10-23 10:30:16 +0200

    Fixes #10, Fixes #11 - Création et implementation du builder pour construire le probleme

--- app/src/main/java/lensjudge/problem/ConstructProblem.java
+++ app/src/main/java/lensjudge/problem/ConstructProblem.java
@@ -1,5 +1,8 @@
 package lensjudge.problem;
 
+import lensjudge.verification.CaseVerification;
+import org.checkerframework.checker.units.qual.C;
+
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
@@ -10,11 +13,11 @@ public class ConstructProblem implements Iterable<TestCase>{
     private List<TestCase> testCases;
     private int timeLimit;
     private int memoryLimit;
-
+    private CaseVerification verification;
 
     @Override
     public Iterator<TestCase> iterator() {
-        return null;
+        return testCases.iterator();
     }
 
     @Override
@@ -26,4 +29,67 @@ public class ConstructProblem implements Iterable<TestCase>{
     public Spliterator<TestCase> spliterator() {
         return Iterable.super.spliterator();
     }
+
+    private ConstructProblem() {
+        this.testCases = null;
+        this.timeLimit = 0;
+        this.memoryLimit = 0;
+        this.verification = new CaseVerification();
+    }
+
+    // ============ GETTERS / SETTERS =====================
+
+
+    public List<TestCase> getTestCases() {
+        return testCases;
+    }
+
+    public int getTimeLimit() {
+        return timeLimit;
+    }
+
+    public int getMemoryLimit() {
+        return memoryLimit;
+    }
+
+    public CaseVerification getVerification() {
+        return verification;
+    }
+
+    // ============ BUILDER =====================
+
+    public static ConstructProblem newInstance() {
+        return new ConstructProblem();
+    }
+
+    public ConstructProblem withTestCases(TestCase... testCases) {
+        this.testCases = Arrays.asList(testCases);
+        return this;
+    }
+
+    public ConstructProblem withTimeLimit(int timeLimit) {
+        this.timeLimit = timeLimit;
+        return this;
+    }
+
+    public ConstructProblem withMemoryLimit(int memoryLimit) {
+        this.memoryLimit = memoryLimit;
+        return this;
+    }
+
+    public ConstructProblem withVerification(CaseVerification verification) {
+        this.verification = verification;
+        return this;
+    }
+
+    /**
+     * Create a new ConstructProblem object
+     *
+     * @return ConstructProblem
+     */
+    public ConstructProblem build() {
+        return this;
+    }
+
+
 }
