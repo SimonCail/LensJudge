3507cb4bc03d6ed24ae59bd7fa6d2ac20196661f
benjamin_cornet <benjamin_cornet@ens.univ-artois.fr>
2024-10-25 10:44:01 +0200

    Fixes #46 - création de la javadoc

--- app/src/main/java/lensjudge/problem/TestCase.java
+++ app/src/main/java/lensjudge/problem/TestCase.java
@@ -2,31 +2,80 @@ package lensjudge.problem;
 
 import java.io.File;
 
+/**
+ * Represents a test case for the problem
+ */
 public class TestCase {
+    /**
+     * Name of the file
+     */
     private String fileName;
+
+    /**
+     * File for the input
+     */
     private File fileIn;
+
+    /**
+     * File for the output
+     */
     private File fileOut;
+
+    /**
+     * Path for the input and output files
+     */
     private String pathFileIn;
+
+    /**
+     * Path for the output file
+     */
     private String pathFileOut;
 
+    /**
+     * Constructor for the test case
+     *
+     * @param fileName Name of the file
+     * @param fileIn File for the input
+     * @param fileOut File for the output
+     */
     public TestCase(String fileName, File fileIn, File fileOut) {
         this.fileName = fileName;
         this.pathFileIn = fileIn.getAbsolutePath();
         this.pathFileOut = fileOut.getAbsolutePath();
     }
 
+    /**
+     * Get the name of the file
+     *
+     * @return String
+     */
     public String getFileName() {
         return fileName;
     }
 
+    /**
+     * Get the file for the input
+     *
+     * @return File
+     */
     public String getPathFileIn() {
         return pathFileIn;
     }
 
+    /**
+     * Get the file for the output
+     *
+     * @return File
+     */
     public String getPathFileOut() {
         return pathFileOut;
     }
 
+    /**
+     * Check if the files exist
+     *
+     * @return boolean
+     */
     public boolean exists () {
         fileIn = new File(pathFileIn);
         fileOut = new File(pathFileOut);
