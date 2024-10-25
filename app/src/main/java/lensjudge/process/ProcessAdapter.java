a66a04e95b84ab7ad798031c6d17eb5d01d9f1df
benjamin_cornet <benjamin_cornet@ens.univ-artois.fr>
2024-10-25 09:19:03 +0200

    Fixes #44 - fin de la javadoc pour les processus

--- app/src/main/java/lensjudge/process/ProcessAdapter.java
+++ app/src/main/java/lensjudge/process/ProcessAdapter.java
@@ -1,18 +1,50 @@
 package lensjudge.process;
 
 import lensjudge.verification.TypeVerification;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 
 import java.io.*;
 
+
+
+/**
+ * Adapter class for the IControlProcess interface that adapts the ProcessBuilder class
+ */
 public class ProcessAdapter implements IControlProcess {
+    private static final Logger LOGGER = LogManager.getLogger(ProcessAdapter.class.getPackageName());
+
+    /**
+     * The ProcessBuilder object
+     */
     private final ProcessBuilder processBuilder;
+
+    /**
+     * The Process object
+     */
     private Process process;
+
+    /**
+     * The error message for when the process has not been started
+     */
     private static final String PROCESSNOTSTARTED = "Proces has not been started.";
 
+    /**
+     * Constructor for the ProcessAdapter
+     *
+     * @param command the command to be executed
+     */
     public ProcessAdapter(String... command) {
         this.processBuilder = new ProcessBuilder(command);
     }
 
+    /**
+     * Get the error output of the process
+     *
+     * @return the error output of the process
+     */
     @Override
     public String getErrorOutput() {
         if (process == null) {
@@ -32,14 +64,29 @@ public class ProcessAdapter implements IControlProcess {
         return errorOutput.toString();
     }
 
+    /**
+     * Set the working directory of the process
+     *
+     * @param directory the working directory of the process
+     */
     public void setDirectory(String directory) {
         processBuilder.directory(new File(directory));
     }
 
+    /**
+     * Get the input stream of the process
+     *
+     * @return the input stream of the process
+     */
     public InputStream getInputStream() {
         return process.getInputStream();
     }
 
+    /**
+     * Get the standard output of the process
+     *
+     * @return the standard output of the process
+     */
     @Override
     public String getStandardOutput() {
         if (process == null) {
@@ -59,34 +106,45 @@ public class ProcessAdapter implements IControlProcess {
         }
     }
 
-
+    /**
+     * Start the process
+     */
     @Override
     public void startProcess() {
         try {
             this.process = processBuilder.start();
         }catch (IOException e){
             e.printStackTrace();
-            System.out.println(PROCESSNOTSTARTED);
+            LOGGER.printf(Level.WARN, PROCESSNOTSTARTED);
         }
     }
 
+    /**
+     * Wait for the process to finish
+     *
+     * @return the exit value of the process
+     */
     @Override
     public int waitForProcess()  {
         int result = 0;
         try {
             result = process.waitFor();
         }catch (InterruptedException e){
-            System.out.println(PROCESSNOTSTARTED);
+            LOGGER.printf(Level.WARN, PROCESSNOTSTARTED);
+            Thread.currentThread().interrupt();
         }
         return result;
     }
 
+    /**
+     * Stop the process
+     */
     @Override
     public void stopProcess() {
         try {
             process.destroy();
         }catch (Exception e){
-            System.out.println(PROCESSNOTSTARTED);
+            LOGGER.printf(Level.WARN,PROCESSNOTSTARTED);
         }
     }
 }
--- app/src/main/java/lensjudge/process/ProcessDecorator.java
+++ app/src/main/java/lensjudge/process/ProcessDecorator.java
@@ -4,34 +4,70 @@ import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+ * Decorator class for the IControlProcess interface that adds a time limit to the process
+ */
 public abstract class ProcessDecorator implements IControlProcess {
+    /**
+     * The process to be decorated
+     */
     private final IControlProcess processControl;
 
+    /**
+     * Constructor for the ProcessDecorator
+     *
+     * @param processControl the process to be decorated
+     */
     public ProcessDecorator(IControlProcess processControl) {
         this.processControl = processControl;
-
     }
 
+    /**
+     * Start the process
+     *
+     * @throws IOException if the process cannot be started
+     */
     @Override
     public void startProcess() throws IOException {
         processControl.startProcess();
     }
 
+    /**
+     * Wait for the process to finish
+     *
+     * @return the exit value of the process
+     * @throws InterruptedException if the process is interrupted
+     */
     @Override
     public int waitForProcess() throws InterruptedException {
         return processControl.waitForProcess();
     }
 
+    /**
+     * Get the standard output of the process
+     *
+     * @return the standard output of the process
+     * @throws IOException if the process cannot be started
+     */
     @Override
     public String getStandardOutput() throws IOException {
         return processControl.getStandardOutput();
     }
 
+    /**
+     * Get the error output of the process
+     *
+     * @return the error output of the process
+     * @throws IOException if the process cannot be started
+     */
     @Override
     public String getErrorOutput() throws IOException {
         return processControl.getErrorOutput();
     }
 
+    /**
+     * Stop the process
+     */
     @Override
     public void stopProcess() {
         processControl.stopProcess();
